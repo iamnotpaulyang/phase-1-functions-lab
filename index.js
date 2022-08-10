c3a8c987d6107649e3b1d955d1da4edad8b0118e
Paul Yang <paulyang00@gmail.com>
2022-08-10 01:06:40 -0400

    commit tree cbb1315a9efa3e07c8a3da90afba2e9ea34c242e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks > 42) {
+        return blocks - 42
+
+    } return 42 - blocks
+}
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264
+}
+function distanceTravelledInFeet(start, destination){
+    if (destination > start) {
+        return (destination - start)*264
+    
+    } return (start - destination)*264 
+}
+function calculatesFarePrice(start, destination){
+    let feet = distanceTravelledInFeet(start, destination)
+        if (feet < 400 ){
+            return 0
+        } else if ( feet > 400 && feet <= 2000){
+            return (feet-400)*0.02 
+    }   else if (feet > 2000 && feet <=2500) {
+            return 25
+    }return 'cannot travel that far'
+}
